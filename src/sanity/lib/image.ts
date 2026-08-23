9eade546315dd3792c7b929052d8a7872e81a117
designedbybami <akinadebolu123@gmail.com>
2026-08-24 00:37:43 +0100

    feat(sanity): add CMS client and image URL helper
    - Add the Sanity client, reading project and dataset from the environment
    - Add urlFor(), the image URL builder used for CDN transforms

---
+++ src/sanity/lib/image.ts
@@ -0,0 +1,6 @@
+import { createImageUrlBuilder, type SanityImageSource } from "@sanity/image-url";
+import { client } from "../client";
+
+const builder = createImageUrlBuilder(client);
+
+export const urlFor = (source: SanityImageSource) => builder.image(source);
